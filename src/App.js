07c052073cd4fdcb348f3e6a9011699223263192
EnChiSu <r07741074@ntu.edu.tw>
2022-10-30 21:23:03 +0800

    setup main page

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import useObserveScroll from './hooks/useObserveScroll';
+import useFetchData from './hooks/useFetchData';
+import Spinner from './components/Spinner';
+import ImageCard from './components/ImageCard';
+
+function App() {
+    const { loadRef, page } = useObserveScroll();
+    const { loading, photos } = useFetchData(page);
+    let index = 1;
+
+    return (
+        <div className="App">
+            <div className="image__container">
+                {photos?.map((photo) => (
+                    <ImageCard key={index++} photo={photo} />
+                ))}
+            </div>
+            <div ref={loadRef}>{loading && <Spinner />}</div>
+        </div>
+    );
+}
+
+export default App;
