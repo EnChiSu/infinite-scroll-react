8f472b0f74861ad0816d03b0c7d839588df30d88
EnChiSu <r07741074@ntu.edu.tw>
2022-11-01 20:55:04 +0800

    add menu bar in the main page

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import useFetchData from './hooks/useFetchData';
 import Spinner from './components/Spinner';
 import ImageCard from './components/ImageCard';
 import BackTopBtn from './components/BackTopBtn';
+import Burger from './components/Burger';
+import Menu from './components/Menu';
+import { useState, useRef } from 'react';
 
 function App() {
     const { loadRef, page } = useObserveScroll();
     const { loading, photos } = useFetchData(page);
+    const [open, setOpen] = useState(false);
+    const node = useRef();
+
     let index = 1;
 
     return (
         <div className="App">
+            <div
+                className='nav__bar' 
+                ref={node}
+            >
+                <Burger open={open} setOpen={setOpen} />
+                <Menu open={open} setOpen={setOpen} />
+            </div>
             <div className="image__container">
                 {photos?.map((photo) => (
                     <ImageCard key={index++} photo={photo} />
