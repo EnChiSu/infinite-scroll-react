68bfc1282f934519821f036095806607b6093127
EnChiSu <r07741074@ntu.edu.tw>
2022-11-28 22:09:24 +0800

    dynamic adjust burger style according to dark theme btn

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -1,6 +1,25 @@
 import './Burger.css';
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Burger = ({ open, setOpen }) => {
+    const mode = useSelector((state) => state.darkMode);
+    const { isdarkMode } = mode;
+
+    useEffect(() => {
+        var elements = document.getElementsByClassName("animate__burger")
+        if(open) {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.background = "white";
+            }
+        } else {
+            for (var i = 0; i < elements.length; i++) {
+                elements[i].style.background = isdarkMode ? "white" : "black";
+            }
+        }
+        
+    }, [isdarkMode]);
+
     return (
         <button 
             className='menu__btn'
@@ -9,15 +28,15 @@ const Burger = ({ open, setOpen }) => {
         >
             <div 
                 className='animate__burger'
-                style={{ background: open ? 'white' : 'black', transform: open ? 'rotate(45deg)' : 'rotate(0)' }}
+                style={{ background: open && !isdarkMode ? 'white' : 'black', transform: open ? 'rotate(45deg)' : 'rotate(0)' }}
             />
             <div 
                 className='animate__burger'
-                style={{ background: open ? 'white' : 'black', transform: open ? 'translateX(20px)' : 'translateX(0)', opacity: open ? '0' : '1' }}
+                style={{ background: open && !isdarkMode ? 'white' : 'black', transform: open ? 'translateX(20px)' : 'translateX(0)', opacity: open ? '0' : '1' }}
             />
             <div 
                 className='animate__burger'
-                style={{ background: open ? 'white' : 'black', transform: open ? 'rotate(-45deg)' : 'rotate(0)' }}
+                style={{ background: open && !isdarkMode ? 'white' : 'black', transform: open ? 'rotate(-45deg)' : 'rotate(0)' }}
             />
         </button>
     )
